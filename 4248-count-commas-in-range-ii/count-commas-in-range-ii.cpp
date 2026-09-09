60c4e42fc2818202917a2158ba6f62783e66cba3
Disha Upadhyay <tuesdayyhater@gmail.com>
2026-09-10 00:53:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (84.80%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long totalCommas =0;
+        long long base =1000;
+
+        while (n>=base) {
+            totalCommas += (n-base+1);
+            if(base > LLONG_MAX/1000) break;
+            base *= 1000;
+        }
+        return totalCommas;
+        
+    }
+};
